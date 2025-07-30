43780629094af73ca0e65df415ba6b8e15bf0cf4
mateusblak5 <mateus.da.rosa@escola.pr.gov.br>
2025-07-30 13:35:02 +0000

    Aula 05, parte 3

--- aula05.py
+++ aula05.py
@@ -16,3 +16,10 @@ print(f'Decis: {decis}')
 # Calcular percentis
 percentis = np.percentile(dados, [10, 25, 50, 75, 90])
 print(f'Percentis: {percentis}')
+
+# Visualização por BoxPlot
+plt.boxplot(dados, vert=False)
+plt.title('Bloxplot das notas')
+plt.xlabel('notas')
+plt.show()
+plt.savefig('chart5.png')
